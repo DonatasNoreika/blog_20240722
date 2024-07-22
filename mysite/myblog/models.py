bd707dce3f978b46e893237b1da7e472cbe93754
DonatasNoreika <donoras@gmail.com>
2024-07-22 21:44:33 +0300

    Aprašyti modeliai

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.forms import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=50)
+    content = models.TextField(verbose_name="Content", max_length=2000)
+    date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
+    author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE)
+    content = models.TextField(verbose_name="Content", max_length=1000)
+    date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
+    author = models.ForeignKey(to=User, verbose_name="Author", on_delete=models.CASCADE)
